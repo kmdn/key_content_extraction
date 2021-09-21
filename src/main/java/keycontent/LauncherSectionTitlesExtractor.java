e012a8fee4386fb9a90e45561df9c93a03c4bf8e
~Kris~ <kristian.noullet@kit.edu>
2021-09-21 13:35:09 +0200

    Added grobid dependencies...

---
+++ src/main/java/keycontent/LauncherSectionTitlesExtractor.java
@@ -0,0 +1,45 @@
+package keycontent;
+
+import java.util.Arrays;
+
+import org.grobid.core.data.BiblioItem;
+import org.grobid.core.engines.Engine;
+import org.grobid.core.factory.GrobidFactory;
+import org.grobid.core.main.GrobidHomeFinder;
+import org.grobid.core.utilities.GrobidProperties;
+
+public class LauncherSectionTitlesExtractor {
+	public static void main(String[] args) {
+		final String pdfFolder = "C:\\Users\\wf7467\\Desktop\\JabRef\\Papers\\2021";
+		final String filename = "2008.05190.pdf";
+		// "2101.09969.pdf";
+		String pdfPath = pdfFolder + "\\" + filename;
+
+		try {
+			String pGrobidHome = "C:\\Users\\wf7467\\Desktop\\GitHub\\KIT\\grobid\\grobid-home";
+
+			// The GrobidHomeFinder can be instantiate without parameters to verify the
+			// grobid home in the standard
+			// location (classpath, ../grobid-home, ../../grobid-home)
+
+			// If the location is customised:
+			GrobidHomeFinder grobidHomeFinder = new GrobidHomeFinder(Arrays.asList(pGrobidHome));
+
+			// The grobid yaml config file needs to be instantiate using the correct
+			// grobidHomeFinder or it will use the default
+			// locations
+			GrobidProperties.getInstance(grobidHomeFinder);
+
+			System.out.println(">>>>>>>> GROBID_HOME=" + GrobidProperties.getGrobidHome());
+
+			Engine engine = GrobidFactory.getInstance().createEngine();
+
+			// Biblio object for the result
+			BiblioItem resHeader = new BiblioItem();
+			String tei = engine.processHeader(pdfPath, 1, resHeader);
+		} catch (Exception e) {
+			// If an exception is generated, print a stack trace
+			e.printStackTrace();
+		}
+	}
+}
